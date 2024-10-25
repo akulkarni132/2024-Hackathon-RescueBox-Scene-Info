2890267dd4bf4b5978cee64884f21209dd9d87dd
Prasanna Lakkur Subramanyam <psubramanyam@umass.edu>
2024-10-25 14:07:33 -0400

    answer_questions function added

--- tool-suite/scene-info/ollama_generate.py
+++ tool-suite/scene-info/ollama_generate.py
@@ -1,13 +1,14 @@
 import ollama
+import json
 
-
-def generate_text(prompt, system_prompt, model="llava", image_path=None):
+def generate_text(prompt, system_prompt=None, model="llava", image_path=None):
     args = {
         "prompt": prompt,
-        "system": system_prompt,
         "model": model,
         "stream": False,
     }
+    if system_prompt:
+        args["system"] = system_prompt
     if image_path:
         args["images"] = [image_path]
     print(args)
@@ -37,8 +38,41 @@ Facts:
 
 GEN_QUESTIONS_SYS_PROMPT = """
 You are given a prompt and an objective. The objective is generally a question that we want answered about the image.
+
+YOU DON'T HAVE ACCESS TO THE IMAGE. Generate the questions that helps with the objective if some human answered
+these questions by looking at the image.
+
 Your job is to generate a list of intermediate questions such that the answers to those questions will help us answer the
 question in the objective. Output the list as a JSON.
+"""
+
+GEN_QUESTIONS_PROMPT= """
+{GEN_QUESTIONS_SYS_PROMPT}
+
+Objective: {prompt}
+"""
+
+GEN_QUESTIONS_SYS_PROMPT = """
+You are given an image and a set of questions. The questions are in a JSON list. Answer each question and return a list of dictionaries.
+
+Example input format:
+[
+"Question 1",
+"Question 2",
+"Question3"
+]
+
+Example output format:
+[
+{"question":"Question 1", "answer": "Answer 1"},
+{"question":"Question 2", "answer": "Answer 2"},
+{"question":"Question 3", "answer": "Answer 3"},
+]
+"""
+
+GEN_QUESTIONS_PROMPT_TEMPLATE = """
+Answer the following questions about the image and return a json output of questions and answers.
+{questions}
 """
 
 
@@ -48,5 +82,8 @@ def generate_questions(prompt, image_path=None, model="llava"):
     )
 
 
-response = generate_questions("Is this image taken indoors or outdoors?", image_path="car-image.jpg")
-print(response)
+def answer_questions(questions, image_path, model="llava"):
+    prompt = GEN_QUESTIONS_PROMPT_TEMPLATE.format(json.dumps(questions))
+    return generate_text(
+        prompt=prompt, image_path=image_path, system_prompt=GEN_QUESTIONS_SYS_PROMPT, model=model
+    )
