13d6562f2dd3540d8fabf59cf1995a58da067f33
Prasanna Lakkur Subramanyam <psubramanyam@umass.edu>
2024-10-25 12:58:47 -0400

    more generic generat

--- tool-suite/scene-info/ollama_generate.py
+++ tool-suite/scene-info/ollama_generate.py
@@ -1,11 +1,14 @@
 import ollama
 
-def generate_text(instruction, file_path, system_prompt, model='llava'):
-    result = ollama.generate(
-        model=model,
-        prompt=instruction,
-        system=system_prompt,
-        images=[file_path],
-        stream=False
-    )['response']
+def generate_text(prompt, system_prompt, model='llava', image_path=None):
+    args = {
+        "prompt": prompt,
+        "system": system_prompt,
+        "model": model,
+        "stream": False,
+    }
+    if image_path:
+        args["images"] = [image_path]
+    
+    result = ollama.generate(**args)['response']
     return result
