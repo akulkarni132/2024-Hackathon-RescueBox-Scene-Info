6bb3d4788c3bdbf13bf27c3128b95abc8c585f90
Prasanna L S <prasanna.lakkur@gmail.com>
2024-10-25 21:31:20 +0530

    Update ollama_generate.py

--- tool-suite/scene-info/ollama_generate.py
+++ tool-suite/scene-info/ollama_generate.py
@@ -12,9 +12,4 @@ def generate_text(instruction, file_path, system_prompt):
         images=[file_path],
         stream=False
     )['response']
-    img=Image.open(file_path, mode='r')
-    img = img.resize([int(i/1.2) for i in img.size])
-    # display(img) 
-    # for i in result.split('.'):
-        # print(i, end='', flush=True)
     return result
